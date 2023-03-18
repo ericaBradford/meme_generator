f502b822b5c9a296b4157eaa561243d42f9768d5
Erica Bradford <ericadenisebradford@gmail.com>
2023-03-18 16:19:36 -0600

    Add in control to change image text color

--- src/components/imageEditor.jsx
+++ src/components/imageEditor.jsx
@@ -10,6 +10,7 @@ import PropTypes from 'prop-types';
 function ImageEditor(props) {
     const [topText, setTopText] = useState('');
     const [bottomText, setBottomText] = useState('');
+    const [textColor, setTextColor] = useState('#000');
 
     const {
         imgUrl
@@ -36,13 +37,30 @@ function ImageEditor(props) {
                         />
                     </div>
                     <div className='form-group'>
-                        <label for='bottom'>Bottom Text</label>
+                        <label for='bottomText'>Bottom Text</label>
                         <input
                             type='text'
-                            name='bottom'
+                            name='bottomText'
                             onChange={event => setBottomText(event.target.value)}
                             value={bottomText}
                         />
+                    </div>
+                    <div className='form-group'>
+                        <label for='textColor'>Text Color</label>
+                        <select id="textColor" onChange={event => setTextColor(event.target.value)}
+                            value={textColor}>
+                            <option value="#000">Black</option>
+                            <option value="#fff">White</option>
+                            <option value="#808080">Grey</option>
+                            <option value="#964B00">Brown</option>
+                            <option value="#ff0000">Red</option>
+                            <option value="#ffa500">Orange</option>
+                            <option value="#ffff00">Yellow</option>
+                            <option value="#008000">Green</option>
+                            <option value="#0000ff">Blue</option>
+                            <option value="#4b0082">Indigo</option>
+                            <option value="#ee82ee">Violet</option>
+                        </select>
                     </div>
                 </form>
             </>;
@@ -56,8 +74,8 @@ function ImageEditor(props) {
     const getImage = function () {
         return urlValid &&
             <div className='meme-container'>
-                <span className='top-meme-text'>{topText}</span>
-                <span className='bottom-meme-text'>{bottomText}</span>
+                <span className='top-meme-text' style={{ color: textColor }}>{topText}</span>
+                <span className='bottom-meme-text' style={{ color: textColor }}>{bottomText}</span>
                 <img src={imgUrl} alt={topText + ' ' + bottomText} />
             </div>;
     };
